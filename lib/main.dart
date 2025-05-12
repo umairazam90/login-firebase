8747d61057eeac6c1b239b72691e8f21f0af2437
Umair Malik <167157598+umairazam90@users.noreply.github.com>
2025-05-12 15:14:07 -0400

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options:const FirebaseOptions(
+          apiKey: "AIzaSyCycRvjlpuR1rPKDyQVDwrBobC0Y78vfOY",
+          authDomain: "fir-login-ad394.firebaseapp.com",
+          projectId: "fir-login-ad394",
+          storageBucket: "fir-login-ad394.firebasestorage.app",
+          messagingSenderId: "276738967348",
+          appId: "1:276738967348:web:5b6702618d717ee3138761")
+  ); // Make sure this is here
   runApp(MyApp());
 }
 
